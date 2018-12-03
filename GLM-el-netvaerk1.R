59564a3d0ad8407c4995a01f61f56a583f825f77
Thomas-Dam-P <tdpe15@student.aau.dk>
2018-12-03 09:17:51 +0100

    Tilføjede kommentarer.

--- GLM-el-netvaerk1.R
+++ GLM-el-netvaerk1.R
@@ -18,21 +18,21 @@ Nulm=matrix(rep(0,676),26,26)
 A=matrix(c(rbind(DTD,C),rbind(t(C),Nulm)),63,63)
 Ainv=solve(A)
 Beta=Ainv%*%c(t(Dtilde)%*%dtilde,rep(0,26))
-Beta[1:37]
+Beta[1:37]#Her er det første estimat for strømstyrker og spændinger i hele netværket:
 for(i in 1:50)
 {
   d=c(effekt/c(Beta[21:22],Beta[25],Beta[27],Beta[29],Beta[31],Beta[33],Beta[35:37]),400)
   dtilde=invsqrtSigma%*%d
   Beta=Ainv%*%c(t(Dtilde)%*%dtilde,rep(0,26))
 }
-Beta[1:37]
+Beta[1:37]#Her har vi divideret med de estimerede spændinger for at finde strømstyrken. (Istedet for bare at dividere dem alle med 400.) Vi itererer til konvergens.
 konf=matrix(rep(0,74),37,2)
 for(i in 1:37)
 {
   konf[i,1]=Beta[i]-1.959963984540*sqrt(Ainv[i,i])
   konf[i,2]=Beta[i]+1.959963984540*sqrt(Ainv[i,i])
 }
-konf
+konf#Konfidensintervaller for spændingerne:
 z=c()
 T=360
 for(i in 19:37)
@@ -40,4 +40,4 @@ for(i in 19:37)
   z=c(z,(Beta[i]-T)/sqrt(Ainv[i,i])) #Wald's test
 }
 z
-pnorm(z)# De giver alle et, da det er meget usandsynligt, at den rigtige værdi ligger under 360V.
+pnorm(z)# p-værdier udregnes. De giver alle tal større end 0.05, så vi kan for hver knude sige med 95% sikkerhed, at den knude ikke har en spænding under 360V.
