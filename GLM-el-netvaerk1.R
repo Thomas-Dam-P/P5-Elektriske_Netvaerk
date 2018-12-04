e2826885a8bfd2e9585664e628bc5177feae25aa
Thomas-Dam-P <tdpe15@student.aau.dk>
2018-12-04 10:14:18 +0100

    rent faktisk tilføjet det jeg skrev før

--- GLM-el-netvaerk1.R
+++ GLM-el-netvaerk1.R
@@ -1,6 +1,6 @@
 library("pracma")
-Sigma=read.table("Sigmany.txt")
-Sigma=as.matrix(Sigma)
+#Sigma=read.table("Sigmany400.txt")
+#Sigma=as.matrix(Sigma)
 #Sigma=Sigma/400^2
 #Sigma[11,11]=Sigma[11,11]*400^2
 C=read.table("Cny.txt")
@@ -10,6 +10,7 @@ D=as.matrix(D)
 effekt=read.table("effekt.txt")
 effekt=as.matrix(effekt)
 d=c(effekt/400,400)
+Sigma=diag((d*0.01)^2)
 invsqrtSigma=solve(sqrt(Sigma))
 dtilde=invsqrtSigma%*%d
 Dtilde=invsqrtSigma%*%D
@@ -19,10 +20,16 @@ A=matrix(c(rbind(DTD,C),rbind(t(C),Nulm)),63,63)
 Ainv=solve(A)
 Beta=Ainv%*%c(t(Dtilde)%*%dtilde,rep(0,26))
 Beta[1:37]#Her er det første estimat for strømstyrker og spændinger i hele netværket:
-for(i in 1:50)
+for(i in 1:500)
 {
   d=c(effekt/c(Beta[21:22],Beta[25],Beta[27],Beta[29],Beta[31],Beta[33],Beta[35:37]),400)
+  Sigma=diag((d*0.01)^2)
+  invsqrtSigma=solve(sqrt(Sigma))
   dtilde=invsqrtSigma%*%d
+  Dtilde=invsqrtSigma%*%D
+  DTD=t(Dtilde)%*%Dtilde
+  A=matrix(c(rbind(DTD,C),rbind(t(C),Nulm)),63,63)
+  Ainv=solve(A)
   Beta=Ainv%*%c(t(Dtilde)%*%dtilde,rep(0,26))
 }
 Beta[1:37]#Her har vi divideret med de estimerede spændinger for at finde strømstyrken. (Istedet for bare at dividere dem alle med 400.) Vi itererer til konvergens.
@@ -32,7 +39,7 @@ for(i in 1:37)
   konf[i,1]=Beta[i]-1.959963984540*sqrt(Ainv[i,i])
   konf[i,2]=Beta[i]+1.959963984540*sqrt(Ainv[i,i])
 }
-konf#Konfidensintervaller for spændingerne:
+konf#Konfidensintervaller for parametrene:
 z=c()
 T=360
 for(i in 19:37)
@@ -41,36 +48,8 @@ for(i in 19:37)
 }
 z
 pnorm(z)# p-værdier udregnes. De giver alle tal større end 0.05, så vi kan for hver knude sige med 95% sikkerhed, at den knude ikke har en spænding under 360V.
-# simdata=rnorm(37,mean=Beta[1:37],sd=sqrt(Beta[1:37]^2)*0.01)
-# Sigma=diag((simdata*0.01)^2)
-# D=diag(rep(1,37))
-# invsqrtSigma=solve(sqrt(Sigma))
-# dtilde=invsqrtSigma%*%simdata
-# Dtilde=invsqrtSigma%*%D
-# DTD=t(Dtilde)%*%Dtilde
-# Nulm=matrix(rep(0,676),26,26)
-# A=matrix(c(rbind(DTD,C),rbind(t(C),Nulm)),63,63)
-# Ainv=solve(A)
-# Beta=Ainv%*%c(t(Dtilde)%*%dtilde,rep(0,26))
-# Beta[1:37]
-# P=Dtilde%*%Ainv[1:37,1:37]%*%t(Dtilde)
-# plot((dtilde-P%*%dtilde)[19:37])#Residualplot i forhold til indeks
-# plot((sqrt(Sigma)%*%(P%*%dtilde))[1:18],(dtilde-P%*%dtilde)[1:18])#residualplot ift fittede værdier
-# P%*%dtilde
-# P%*%simdata
-# P
-# (dtilde-P%*%dtilde)#residualer på den ene måde
-# dtilde-Dtilde%*%Beta[1:37]#residualer på den anden måde
-# Beta[1:18]
-# (sqrt(Sigma)%*%(P%*%dtilde))[1:18]
-# lev=c()
-# for(i in 1:37)
-# {
-#   lev=c(lev,P[i,i])
-# }
-# remove=match(min(lev),lev)
-simdata=rnorm(37,mean=Beta[1:37],sd=sqrt(Beta[1:37]^2)*0.01)
-Sigma=diag(37)#diag((Beta[1:37]*0.01)^2)
+simdata=rnorm(37,mean=Beta[1:37],sd=abs(Beta[1:37])*0.01)
+Sigma=diag((Beta[1:37]*0.01)^2)#diag(37)
 D=diag(rep(1,37))
 invsqrtSigma=solve(sqrt(Sigma))
 dtilde=invsqrtSigma%*%simdata
@@ -116,6 +95,7 @@ for(i in 1:37)
     {
       lev=c(lev,P[j,j])
     }
+    print(lev)
   }
 }
 D
@@ -133,3 +113,31 @@ D
 # dtilde-Dtilde%*%Beta[1:37]
 # dtilde-Dtilde%*%Ainv[1:37,1:37]%*%t(Dtilde)%*%dtilde
 # plot(1:11,dtilde-Dtilde%*%Ainv[1:37,1:37]%*%t(Dtilde)%*%dtilde)
+# simdata=rnorm(37,mean=Beta[1:37],sd=sqrt(Beta[1:37]^2)*0.01)
+# Sigma=diag((simdata*0.01)^2)
+# D=diag(rep(1,37))
+# invsqrtSigma=solve(sqrt(Sigma))
+# dtilde=invsqrtSigma%*%simdata
+# Dtilde=invsqrtSigma%*%D
+# DTD=t(Dtilde)%*%Dtilde
+# Nulm=matrix(rep(0,676),26,26)
+# A=matrix(c(rbind(DTD,C),rbind(t(C),Nulm)),63,63)
+# Ainv=solve(A)
+# Beta=Ainv%*%c(t(Dtilde)%*%dtilde,rep(0,26))
+# Beta[1:37]
+# P=Dtilde%*%Ainv[1:37,1:37]%*%t(Dtilde)
+# plot((dtilde-P%*%dtilde)[19:37])#Residualplot i forhold til indeks
+# plot((sqrt(Sigma)%*%(P%*%dtilde))[1:18],(dtilde-P%*%dtilde)[1:18])#residualplot ift fittede værdier
+# P%*%dtilde
+# P%*%simdata
+# P
+# (dtilde-P%*%dtilde)#residualer på den ene måde
+# dtilde-Dtilde%*%Beta[1:37]#residualer på den anden måde
+# Beta[1:18]
+# (sqrt(Sigma)%*%(P%*%dtilde))[1:18]
+# lev=c()
+# for(i in 1:37)
+# {
+#   lev=c(lev,P[i,i])
+# }
+# remove=match(min(lev),lev)
